fd3e50bc0566c7799c7acc47fa90c54acbbc5288
Kelvin Tse <post@ktml.net>
2014-07-13 13:09:42 +1000

    Merged hugh code. Merged addLayers into script.js

--- kttestcode/addLayers.js
+++ kttestcode/addLayers.js
@@ -1,5 +1,160 @@
-//Loads the data layers ready for adding to the map
+var layer_pnts = [
+    {
+    	"filename": "pntsAir",
+    	"imagename": "icon_transport_air"
+    },
+    {
+    	"filename": "pntsBushfire",
+    	"imagename": "icon_natural_bushfire"
+    },
 
+    {
+    	"filename": "pntsCyclone",
+    	"imagename": "icon_natural_cyclone"
+    },
+    {
+    	"filename": "pntsEarthquake",
+    	"imagename": "icon_natural_earthquake"
+    },
+    {
+    	"filename": "pntsFire",
+    	"imagename": "icon_manmade_fire"
+    },
+    {
+    	"filename": "pntsFlood",
+    	"imagename": "icon_natural_flood"
+    },
+    {
+    	"filename": "pntsHeatwave",
+    	"imagename": "icon_natural_heatwave"
+    },
+    {
+    	"filename": "pntsIndustrial",
+    	"imagename": "icon_manmade_industrial"
+    },
+    {
+    	"filename": "pntsLandslide",
+    	"imagename": "icon_natural_landslide"
+    },
+    {
+    	"filename": "pntsRail",
+    	"imagename": "icon_transport_rail"
+    },
+    {
+    	"filename": "pntsRiptide",
+    	"imagename": "icon_natural_riptide"
+    },
+    {
+    	"filename": "pntsRoad",
+    	"imagename": "icon_transport_road"
+    },
+    {
+    	"filename": "pntsStormHail",
+    	"imagename": "icon_natural_storm"
+    },
+    {
+    	"filename": "pntsWater",
+    	"imagename": "icon_transport_water"
+    }
+];
+
+var pnts_layers = [];
+var pnts = [];
+
+for(var i=0;i<layer_pnts.length;i++){
+    var obj = layer_pnts[i];
+    pnts_layers.push(readcsv(obj.filename,obj.imagename));
+}
+
+String.prototype.repeat = function( num )
+{
+    return new Array( num + 1 ).join( this );
+}
+
+function readcsv(filename, imagename) {
+	var pnts = L.geoJson(null, {
+		onEachFeature: onEachFeature,
+		
+		pointToLayer: function (feature, latlng) {
+
+            var showdeath_max = 35;
+            var showinjuries_max = 30;
+            var showhomes_max = 16;
+
+            var icon_img = '<img src="images/' + imagename + '">';
+            var icon_class = imagename;
+            var name = 'Example name';
+            var description = 'Donec id elit non mi porta gravida at eget metus. Donec sed odio dui.';
+            var link = "http://google.com.au";
+            var death_num = 35;
+            var injuries_num = 35;
+            var homes_num = 15;
+            var dollars_lost = '$13,000';
+
+
+            var death_icons = '<span class="icon death"></span>'.repeat(Math.min(death_num,showdeath_max));
+            var injuries_icons = '<span class="icon injury"></span>'.repeat(Math.min(injuries_num,showinjuries_max));
+            var homes_icons = '<span class="icon home"></span>'.repeat(Math.min(homes_num,showhomes_max));
+
+            if (death_num > showdeath_max) { death_icons += '<span class="more">...</span>'; }
+            if (injuries_num > showinjuries_max) { injuries_icons += '<span class="more">...</span>'; }
+            if (homes_num > showhomes_max) { homes_icons += '<span class="more">...</span>'; }
+
+            death_icons = '<span class="value">' + death_icons + '</span>';
+            injuries_icons = '<span class="value">' + injuries_icons + '</span>';
+            homes_icons = '<span class="value">' + homes_icons + '</span>';
+
+            var title_html = '<h3>' + name + '</h3>';
+            var death_row = '<p class="row death"><span class="label">Death</span>' + death_icons + '<span class="num">' + death_num + '</span></p>';
+            var injuries_row = '<p class="row injuries"><span class="label">injuries</span>' + injuries_icons + '<span class="num">' + injuries_num + '</span></p>';
+            var homes_row = '<p class="row homes"><span class="label">Home Destroyed</span>' + homes_icons + '<span class="num">' + homes_num + '</span></p>';
+            var lost_row = '<p class="row lost"><span class="label">Lost in Dollars</span><span class="value">' + dollars_lost + '</span></p>';
+            var desc_row = '<p class="row description">' + description + ' <a href="' + link + '">more</a></p>'
+            var close_button = '<span class="close"></span>';
+
+            var popup_content = title_html + death_row + injuries_row + homes_row + lost_row + desc_row + close_button;
+
+            var myicon = L.divIcon({
+                className: icon_class,
+                html: icon_img
+            });
+
+            var marker = L.marker(latlng, {
+                icon: myicon
+            });
+
+            marker.bindPopup(popup_content).openPopup();
+
+			return marker;
+		}
+	});
+
+	var range = L.geoJson(null, {
+	    onEachFeature: onEachFeature,
+	    
+	    pointToLayer: function (feature, latlng) {
+	        var circle = L.circleMarker(latlng, {
+	            radius: 50,
+	            fillColor: "#e30202",
+	            weight: 0,
+	            opacity: 1,
+	            fillOpacity: 0.1
+	        });
+
+	        map.on('zoomend', function() {
+	            var currentZoom = map.getZoom();
+	            circle.setRadius(currentZoom * 10);
+	        });
+	        return circle;
+	    }
+
+	}); 
+
+	return {'marker':omnivore.csv(filename + '.csv', null, pnts), 'range':omnivore.csv(filename + '.csv', null, range)};
+}
+
+//Loads the data layers ready for adding to the map
+/*
 var pntsAir = L.geoJson(null, {
 	onEachFeature: onEachFeature,
 	
@@ -140,4 +295,4 @@ var pntsWater = L.geoJson(null, {
 	}
 });
 var layerWater = omnivore.csv('pntsWater.csv', null, pntsWater);
-
+*/
--- kttestcode/script.js
+++ kttestcode/script.js
@@ -4,6 +4,93 @@ var info;
 var startZoom = 5;
 var geojsonLayer;
 
+var layer_pnts = [
+    {
+        "filename": "pntsAir",
+        "imagename": "icon_transport_air",
+        "color": "#fda128",
+        "radius": 50
+    },
+    {
+        "filename": "pntsBushfire",
+        "imagename": "icon_natural_bushfire",
+        "color": "#18a762",
+        "radius": 50
+    },
+
+    {
+        "filename": "pntsCyclone",
+        "imagename": "icon_natural_cyclone",
+        "color": "#18a762",
+        "radius": 50
+    },
+    {
+        "filename": "pntsEarthquake",
+        "imagename": "icon_natural_earthquake",
+        "color": "#18a762",
+        "radius": 50
+    },
+    {
+        "filename": "pntsFire",
+        "imagename": "icon_manmade_fire",
+        "color": "#e30202",
+        "radius": 50
+    },
+    {
+        "filename": "pntsFlood",
+        "imagename": "icon_natural_flood",
+        "color": "#18a762",
+        "radius": 50
+    },
+    {
+        "filename": "pntsHeatwave",
+        "imagename": "icon_natural_heatwave",
+        "color": "#18a762",
+        "radius": 50
+    },
+    {
+        "filename": "pntsIndustrial",
+        "imagename": "icon_manmade_industrial",
+        "color": "#e30202",
+        "radius": 50
+    },
+    {
+        "filename": "pntsLandslide",
+        "imagename": "icon_natural_landslide",
+        "color": "#18a762",
+        "radius": 50
+    },
+    {
+        "filename": "pntsRail",
+        "imagename": "icon_transport_rail",
+        "color": "#fda128",
+        "radius": 50
+    },
+    {
+        "filename": "pntsRiptide",
+        "imagename": "icon_natural_riptide",
+        "color": "#18a762",
+        "radius": 50
+    },
+    {
+        "filename": "pntsRoad",
+        "imagename": "icon_transport_road",
+        "color": "#fda128",
+        "radius": 50
+    },
+    {
+        "filename": "pntsStormHail",
+        "imagename": "icon_natural_storm",
+        "color": "#18a762",
+        "radius": 50
+    },
+    {
+        "filename": "pntsWater",
+        "imagename": "icon_transport_water",
+        "color": "#fda128",
+        "radius": 50
+    }
+];
 
 function init() {
     //Initialize the map on the "map" div
@@ -38,22 +125,131 @@ function init() {
 
     //Add the tiled map layer to the map
     map.addLayer(tiles);
+
+    var pnts_layers = [];
+    var pnts = [];
+
+    for(var i=0;i<layer_pnts.length;i++){
+        var obj = layer_pnts[i];
+        pnts_layers.push(readcsv(obj));
+    }
+
+    String.prototype.repeat = function( num )
+    {
+        return new Array( num + 1 ).join( this );
+    }
+
+    function readcsv(obj) {
+        var filename = obj.filename;
+        var imagename = obj.imagename;
+        var color = obj.color;
+        var radius = obj.radius;
+        var pnts = L.geoJson(null, {
+            onEachFeature: onEachFeature,
+            
+            pointToLayer: function (feature, latlng) {
+
+                var showdeath_max = 35;
+                var showinjuries_max = 30;
+                var showhomes_max = 16;
+
+                var icon_img = '<img src="images/' + imagename + '">';
+                var icon_class = imagename;
+                var name = 'Example name';
+                var description = 'Donec id elit non mi porta gravida at eget metus. Donec sed odio dui.';
+                var link = "http://google.com.au";
+                var death_num = 35;
+                var injuries_num = 35;
+                var homes_num = 15;
+                var dollars_lost = '$13,000';
+
+
+                var death_icons = '<span class="icon death"></span>'.repeat(Math.min(death_num,showdeath_max));
+                var injuries_icons = '<span class="icon injury"></span>'.repeat(Math.min(injuries_num,showinjuries_max));
+                var homes_icons = '<span class="icon home"></span>'.repeat(Math.min(homes_num,showhomes_max));
+
+                if (death_num > showdeath_max) { death_icons += '<span class="more">...</span>'; }
+                if (injuries_num > showinjuries_max) { injuries_icons += '<span class="more">...</span>'; }
+                if (homes_num > showhomes_max) { homes_icons += '<span class="more">...</span>'; }
+
+                death_icons = '<span class="value">' + death_icons + '</span>';
+                injuries_icons = '<span class="value">' + injuries_icons + '</span>';
+                homes_icons = '<span class="value">' + homes_icons + '</span>';
+
+                var title_html = '<h3>' + name + '</h3>';
+                var death_row = '<p class="row death"><span class="label">Death</span>' + death_icons + '<span class="num">' + death_num + '</span></p>';
+                var injuries_row = '<p class="row injuries"><span class="label">injuries</span>' + injuries_icons + '<span class="num">' + injuries_num + '</span></p>';
+                var homes_row = '<p class="row homes"><span class="label">Home Destroyed</span>' + homes_icons + '<span class="num">' + homes_num + '</span></p>';
+                var lost_row = '<p class="row lost"><span class="label">Lost in Dollars</span><span class="value">' + dollars_lost + '</span></p>';
+                var desc_row = '<p class="row description">' + description + ' <a href="' + link + '">more</a></p>'
+                var close_button = '<span class="close"></span>';
+
+                var popup_content = title_html + death_row + injuries_row + homes_row + lost_row + desc_row + close_button;
+
+                var myicon = L.divIcon({
+                    className: icon_class,
+                    html: icon_img
+                });
+
+                var marker = L.marker(latlng, {
+                    icon: myicon
+                });
+
+                marker.bindPopup(popup_content).openPopup();
+
+                return marker;
+            }
+        });
+
+        var radius_circle = L.geoJson(null, {
+            onEachFeature: onEachFeature,
+            
+            pointToLayer: function (feature, latlng) {
+                var circle = L.circleMarker(latlng, {
+                    radius: radius,
+                    fillColor: color,
+                    weight: 0,
+                    opacity: 1,
+                    fillOpacity: 0.1
+                });
+
+                map.on('zoomend', function() {
+                    var currentZoom = map.getZoom();
+                    circle.setRadius(currentZoom * 10);
+                });
+                return circle;
+            }
+
+        }); 
+
+        return {'marker':omnivore.csv(filename + '.csv', null, pnts), 'radius':omnivore.csv(filename + '.csv', null, radius_circle)};
+    }
     
-    //Replace with user driven action
-    layerAir.addTo(map);
-    layerBushfire.addTo(map);
-    layerCyclone.addTo(map);
-    layerEarthquake.addTo(map);
-    layerFire.addTo(map);
-    layerFlood.addTo(map);
-    layerHeatwave.addTo(map);
-    layerIndustrial.addTo(map);
-    layerLandslide.addTo(map);
-    layerRail.addTo(map);
-    layerRiptide.addTo(map);
-    layerRoad.addTo(map);
-    layerStormHail.addTo(map);
-    layerWater.addTo(map);
+    for(var i=0;i<pnts_layers.length;i++){
+        pnts_layers[i]['marker'].addTo(map);
+    }
+
+
+    for(var i=0;i<pnts_layers.length;i++){
+        pnts_layers[i]['radius'].addTo(map);
+    }
+
+
+    // Replace with user driven action
+    // layerAir.addTo(map);
+    // layerBushfire.addTo(map);
+    // layerCyclone.addTo(map);
+    // layerEarthquake.addTo(map);
+    // layerFire.addTo(map);
+    // layerFlood.addTo(map);
+    // layerHeatwave.addTo(map);
+    // layerIndustrial.addTo(map);
+    // layerLandslide.addTo(map);
+    // layerRail.addTo(map);
+    // layerRiptide.addTo(map);
+    // layerRoad.addTo(map);
+    // layerStormHail.addTo(map);
+    // layerWater.addTo(map);
 
 /*
 
